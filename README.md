d56cb74d70f52ff77070d5aec90587b86851c3c6
mursona <mursona>
2023-11-04 16:41:59 +0600

    readme file add

--- README.md
+++ README.md
@@ -1,70 +1,23 @@
-# Getting Started with Create React App
+# OIGallery - Image Gallery for Ollyo Job Task
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+### `OIGallery`
 
-## Available Scripts
+Description of project:
 
-In the project directory, you can run:
+* Grid layout for the image gallery.
+* The gallery Feature Image larger than the others.
+* User can reorder images in the gallery.
+* User can drag-and-drop for reordering.
+* User can select multiple images and delete them.
+* User can set a feature image by sorting.
+* Responsive Image Gallery
 
-### `npm start`
+### `Technologies & Packages`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+* React JS
+* React Icon and router-dom
+* Tailwind CSS 
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## [Client Side live link](https://oigallery.netlify.app/)
+Click here for the private repo: [https://oigallery.netlify.app/](https://oigallery.netlify.app/)
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+
 // react-icon package
 import { BsImages } from 'react-icons/bs';
 import { BsCheckSquareFill } from 'react-icons/bs';
 import { MdMenu } from 'react-icons/md';
 import { IoMdClose } from 'react-icons/io';
+
 // import images from asset
 import img1 from '../../assets/images/image-1.webp';
 import img2 from '../../assets/images/image-2.webp';
@@ -17,7 +19,6 @@ import img9 from '../../assets/images/image-9.webp';
 import img10 from '../../assets/images/image-10.jpeg';
 import img11 from '../../assets/images/image-11.jpeg';
 import logo from '../../assets/images/logo.svg';
-
 
 const Gallery = () => {
 
@@ -104,13 +105,10 @@ const Gallery = () => {
         <div>
         
         {/* start delete and select Navbar */}
-        <div className="px-4 py-2 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 bg-white rounded-t-lg">
+        <div className="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 bg-white rounded-t-lg">
             <div className='grid justify-between grid-cols-2'>
             <ul className="items-center hidden lg:flex">
-            <li>
-                <div
-                className="flex items-center font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                >
+            <li  className="flex items-center font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
                 {
                   selectedRows.length === 0 ?
                   <span className='flex items-center text-left'>
@@ -123,64 +121,67 @@ const Gallery = () => {
                   <p>{selectedRows.length} Files Sletected</p>
                   </span>
                 }
-                </div>
             </li>
             </ul>
             <ul className="items-center hidden ml-auto lg:flex">
             <li>
             <button
-                
-                className="inline-flex items-center justify-center w-full h-10 px-6 font-medium tracking-wide text-gray-700 transition duration-200 rounded shadow-md bg-gray-100 hover:bg-white focus:shadow-outline focus:outline-none"
+                className="inline-flex items-center justify-center w-full h-8 px-4 font-medium tracking-wide text-gray-700 transition duration-300 ease-out hover:ease-in rounded shadow-md bg-gray-100 hover:bg-blue-500 hover:text-white"
                 onClick={handleDeleteRows}
                 title="Delete Files">
                 {
                   selectedRows.length === 0 ? 
                   "Selete Files" : "Delete Files"
                 }
-               
             </button>
             </li>
             </ul>
             </div>
+
+            {/*slect and delete nav responsive for mobile and tablet  with open and close state */}
             <div className="flex items-center justify-between mx-auto lg:hidden">
             <img className='w-8 h-5' src={logo} alt="" />
+
+            {/* open menu for mobile device */}
             <button
                 aria-label="Open Menu"
                 title="Open Menu"
-                className="p-2 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+                className="p-2 transition duration-300 ease-out hover:ease-in rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
                 onClick={() => setIsMenuOpen(true)}
             >
             <MdMenu className='text-xl'></MdMenu>
             </button>
+            
             {isMenuOpen && (
                 <div className="absolute top-0 left-0 z-index-3 w-full">
                 <div className="p-5 bg-white border rounded shadow-sm">
                     <div className="flex items-center justify-between mb-4">
+
+                        {/* close menu */}
                         <button
                         aria-label="Close Menu"
                         title="Close Menu"
-                        className="p-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                        className="p-2 -mr-2 transition duration-300 ease-out hover:ease-in rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                         onClick={() => setIsMenuOpen(false)}
                         >
                         <IoMdClose></IoMdClose>
                         </button>
-                        <nav>
-                    <ul className="lg:space-y-4 space-y-0 flex items-center justify-between">
-                        <li>
-                        <p
-                        className="flex items-center font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                        >
 
+                        <nav>
+                        <ul className="lg:space-y-4 space-y-0 flex items-center justify-between">
+                      {/* start select image count for mobile device */} 
+                        <li className="flex items-center font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
                           <span className='flex items-center'>
                           <BsCheckSquareFill className='ml-3 mr-1 text-blue-600'></BsCheckSquareFill>
                           <p>{selectedRows.length} Files Sletected</p>
                           </span>
-                        
-                        </p>
                         </li>
+                        {/* end select image count for mobile device */} 
+
+                        {/* start delete selected image for mobile device */} 
                         <li>
                         <button
-                            className="inline-flex items-center justify-center w-full lg:h-12 h-10 px-6 ml-2 font-medium tracking-wide text-gray-700 transition duration-200 rounded shadow-md bg-gray-100 hover:bg-white focus:shadow-outline focus:outline-none"
+                            className="inline-flex items-center justify-center w-full h-8 px-4 font-medium tracking-wide text-gray-700 transition duration-300 ease-out hover:ease-in rounded shadow-md bg-gray-100 hover:bg-blue-500 hover:text-white ml-2"
                             onClick={handleDeleteRows}
                             title="Delete Files">
                             {
@@ -189,6 +190,7 @@ const Gallery = () => {
                             }
                         </button>
                         </li>
+                        {/* end delete slected image for mobile device*/} 
                     </ul>
                     </nav>
                     </div>
@@ -228,7 +230,7 @@ const Gallery = () => {
 
                 ))}
                 {/* add images card  */}
-                <div className="col-span-1 rounded-lg shadow-xl border hover:bg-gray-900 inset-0 hover:border-none text-center justify-center align-middle px-auto py-auto">
+                <div className="col-span-1 rounded-lg shadow-xl border hover:bg-gray-900 inset-0 hover:border-none text-center justify-center align-middle px-auto py-auto hover:text-white">
                     <div className='my-20' >
                     <BsImages className='text-2xl text-gray-700 mx-auto mb-2'></BsImages>
                    <p>Add Images</p>
